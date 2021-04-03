d90438e377f81d2ec23337f33cbefcd872faf921
Dmytro Hopko <gopkodimon@gmail.com>
2021-04-03 13:23:56 +0300

    Готовая домашняя работа

--- js/script.js
+++ js/script.js
@@ -5,6 +5,6 @@ let deposit;
 let mission;
 let period;
 
-alert ('Владиславе Микалайовичу, радий вітати Вас на моїй сторінці )');
+alert ('Владиславе Микoлайовичу, радий вітати Вас на моїй сторінці )');
 
 console.log("Thaks you for looking my home work) My white niiga<3 ")
