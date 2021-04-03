c9356dcb07da716f931d31a8da649372864c651f
Dmytro Hopko <gopkodimon@gmail.com>
2021-04-03 12:59:23 +0300

    Первый комит

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let money;
+let income;
+let addExpenses;
+let deposit;
+let mission;
+let period;
+
+alert ('Владиславе Микалайовичу, радий вітати Вас на моїй сторінці )');
+
+console.log("Thaks you for looking my home work) My white niiga<3 ")
