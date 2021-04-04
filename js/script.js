df7bf34ca4a4a76006e39f345fada4c2db789df1
Dmytro Hopko <gopkodimon@gmail.com>
2021-04-04 16:38:55 +0300

    ready home task

--- js/script.js
+++ js/script.js
@@ -1,10 +1,35 @@
-let money;
+/* let money;
 let income;
 let addExpenses;
 let deposit;
 let mission;
 let period;
 
-alert ('Владиславе Микалайовичу, радий вітати Вас на моїй сторінці )');
+alert("Владиславе Микалайовичу, радий вітати Вас на моїй сторінці )");
 
-console.log("Thaks you for looking my home work) My white niiga<3 ")
+console.log("Thaks you for looking my home work) My white niiga<3 "); */
+
+// lesson 2 (HOMEWORK)
+
+let money = 54000;
+let income = "фриланс";
+let eddExpenses = "Интернет, Такси, Комуналка";
+let deposit = true;
+let mission = 100100;
+let period = 2;
+
+console.log('money:', typeof money);
+console.log('income:', typeof income);
+console.log('deposite:', typeof deposite);
+
+console.log('Длина строки eddExpenses:', eddExpenses.length);
+
+console.log('Период равен', period, 'месяцев' );
+console.log('Цель зароботать', mission, 'гривен');
+console.log(eddExpenses.toLowerCase());
+console.log(eddExpenses.split(', '));
+
+let budgetDay = money/30;
+console.log('Дневной бюджет:', budgetDay);
+
+
