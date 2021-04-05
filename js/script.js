d24bcad8805b3aa6127db25bfeee599424e3b6c9
Dmytro Hopko <gopkodimon@gmail.com>
2021-04-05 23:09:12 +0300

    Ready home task for lesson 3

--- js/script.js
+++ js/script.js
@@ -31,4 +31,29 @@ console.log(addExpenses.toLowerCase().split(', '));
 let budgetDay = money/30;
 console.log('Дневной бюджет:', budgetDay);
 
+// lesson03 (HOMEWORK)
+money = prompt('Ваш месячный доход?');  // string
+addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+deposit = confirm('Есть ли у вас депозит в банке?');
+
+let expenses1 = prompt('Введите обязательную статью расходов?');
+let amount1 = prompt('Во сколько это обойдется?');
+let expenses2 = prompt('Введите обязательную статью расходов?');
+let amount2 = prompt('Во сколько это обойдется?');
+
+let budgetMonth = +money - +amount1 - +amount2;
+mission = mission / budgetMonth;
+console.log(Math.ceil(mission));
+
+budgetDay = budgetMonth / 30;
+console.log(Math.floor(budgetDay));
+
+
+if (budgetDay >= 1200) console.log('У вас высокий уровень дохода');
+if (budgetDay >= 600 && budgetDay < 1200) console.log('У вас средний уровень дохода');
+if (budgetDay < 600 && budgetDay > 0) console.log(' сожалению у вас уровень дохода ниже среднего');
+if (budgetDay <= 0) console.log('Что то пошло не так');
+
+
+
 
