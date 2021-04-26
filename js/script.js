a7473c5c4efed62b739497909589e430026cacf9
Dmytro Hopko <gopkodimon@gmail.com>
2021-04-26 21:01:59 +0300

    rename variables like in course

--- js/script.js
+++ js/script.js
@@ -153,28 +153,26 @@ appData.calcSavedMoney();
 console.log(capitalize(appData.addExpenses.join(", ")));
  */
 
-let calculateButton = document.getElementById('start'); // a
-let btnPlusIncome = document.getElementsByTagName('button')[0] //b 
-let btnPlusExpenses = document.getElementsByTagName('button')[1]; //b
-let checkbox = document.querySelector('#deposit-check'); // c
-let areaAdditionalIncome = document.querySelectorAll('.additional_income-item'); //d
+let start1 = document.getElementById('start'); //
+let incomePlus = document.getElementsByTagName('button')[0] // 
+let expensesPlus = document.getElementsByTagName('button')[1]; //
+let checdepositCheck = document.querySelector('#deposit-check'); //
+let additionalIncome = document.querySelectorAll('.additional_income-item'); //
+let budgetMonthValue = document.getElementsByClassName('result-total')[0];//
+let budgetDayValue = document.getElementsByClassName('result-total')[1]; //
+let expensesMonthValue = document.getElementsByClassName('result-total')[2];//
+let accumulatedMonthValue = 0; //result-total income_period-value
+let additionalIncomeValue = document.getElementsByClassName('result-total')[3];//
+let additionalExpensesValue = document.getElementsByClassName('result-total')[4];//
+let incomePeriodValue = document.getElementsByClassName('result-total')[5];//
+let targetMonthValue = document.getElementsByClassName('result-total')[6];//
 
-// e
-// let rightPart = document.getElementsByClassName('result-total'); 
-// console.log(rightPart)
-let budgetMonth = document.getElementsByClassName('result-total')[0];
-let budgetDay = document.getElementsByClassName('result-total')[1];
-let expensesMonth = document.getElementsByClassName('result-total')[2];
-let additionalIncome = document.getElementsByClassName('result-total')[3];
-let additionalExpenses = document.getElementsByClassName('result-total')[4];
-let incomePeriod = document.getElementsByClassName('result-total')[5];
-let target_month = document.getElementsByClassName('result-total')[6];
-
-let salaryAmount = document.querySelector('.salary-amount'); // f
-let areaExpensesName = document.querySelectorAll('.expenses-title')[1]; //f
-let areaExpensesSum = document.querySelector('.expenses-amount'); //f
-let areaIncomeTitle = document.querySelectorAll('.income-title')[1];  // f
-let areaIncomeAmount = document.querySelector('.income-amount'); //f
-let areaAdditionalExpenses = document.querySelector('.additional_expenses-item') //f
-let areaTargetAmount = document.querySelector('.target-amount') //f
-let range = document.querySelector('.period-select') //f
+let salaryAmount = document.querySelector('.salary-amount'); //
+let expensesTitle = document.querySelectorAll('.expenses-title')[1]; //
+let expensesAmount = document.querySelector('.expenses-amount');  //
+let incomeTitle = document.querySelectorAll('.income-title')[1];  //
+let incomeAmount = document.querySelector('.income-amount'); //
+let additionalExpenses = document.querySelector('.additional_expenses-item') //
+let areaTargetAmount = document.querySelector('.target-amount') 
+let periodSelect = document.querySelector('.period-select') //
+console.log(areaTargetAmount)
